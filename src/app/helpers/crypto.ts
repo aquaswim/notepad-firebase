6187f8366e3d253a6a9dde73390e619173d1c45f
Bayu Setiawan (BS) <bayusetiawan37@gmail.com>
2021-09-04 01:41:05 +0700

    crypto helper

--- src/app/helpers/crypto.ts
+++ src/app/helpers/crypto.ts
@@ -1,9 +1,18 @@
 import * as CryptoJS from 'crypto-js';
 
+const KEYSEPARATOR = '.';
+
 export function encrypt(str: string, password: string): string {
-  return CryptoJS.AES.encrypt(str, password).toString();
+  const hmac = CryptoJS.HmacMD5(str, password).toString();
+  return CryptoJS.AES.encrypt(`${hmac}${KEYSEPARATOR}${str}`, password).toString();
 }
 
 export function decrypt(str: string, password: string): string | null {
-  return CryptoJS.AES.decrypt(str, password).toString(CryptoJS.enc.Utf8);
+  const data = CryptoJS.AES.decrypt(str, password).toString(CryptoJS.enc.Utf8);
+  const hmac = data.slice(0, data.indexOf(KEYSEPARATOR));
+  const content = data.slice(hmac.length + 1); // remove the KEYSEPARATOR
+  if (hmac !== CryptoJS.HmacMD5(content, password).toString()) {
+    return null;
+  }
+  return content;
 }
