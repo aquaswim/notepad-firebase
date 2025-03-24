628e38c5a2ec5cd0d2d064a696c26b32f8db9031
Aquaswim <bayusetiawan37@gmail.com>
2025-03-24 11:03:25 +0700

    chore: use .env for firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,17 @@
 import {initializeApp} from 'firebase/app';
 import {getDatabase} from 'firebase/database';
 
+const env = import.meta.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBx8SYxScfrSRYIpShgkv0IO8ind9QnpgQ",
-    authDomain: "firenote-18b62.firebaseapp.com",
-    databaseURL: "https://firenote-18b62.firebaseio.com",
-    projectId: "firenote-18b62",
-    storageBucket: "firenote-18b62.appspot.com",
-    messagingSenderId: "834865327455",
-    appId: "1:834865327455:web:4000e4c3740cb0f31c5aea",
-    measurementId: "G-Z3J7CY9PC5"
+    apiKey: env.VITE_FIREBASE_APIKEY,
+    authDomain: env.VITE_FIREBASE_AUTHDOMAIN,
+    databaseURL: env.VITE_FIREBASE_DATABASEURL,
+    projectId: env.VITE_FIREBASE_PROJECTID,
+    storageBucket: env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: env.VITE_FIREBASE_APPID,
+    measurementId: env.VITE_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
