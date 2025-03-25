4d59a601c682aae7575041afef42db792cfc2f48
aquaswim <bayusetiawan37@gmail.com>
2025-03-25 16:16:16 +0700

    feat: basic editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Container from "./Container.tsx";
 import {getCurrentDocID} from "./utils.ts";
 import Home from "./Home.tsx";
-import TextBox from "./TextBox.tsx";
+import Editor from "./Editor.tsx";
 
 function App() {
     const docId = getCurrentDocID();
     return (
         <Container fluid>
-            {!docId ? <Home/> : <TextBox documentID={docId}/>}
+            {!docId ? <Home/> : <Editor/>}
         </Container>
     )
 }
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,16 +1,18 @@
-// import {$getRoot, $getSelection} from 'lexical';
-// import {useEffect} from 'react';
-
+import {useEffect} from 'react';
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
-
-import './editor.css';
-import {OnChangePlugin} from "@lexical/react/LexicalOnChangePlugin";
-import {EditorState} from "lexical";
+import './Editor.css';
+import {PlainTextPlugin} from "@lexical/react/LexicalPlainTextPlugin";
+import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
+import {$generateNodesFromDOM, $generateHtmlFromNodes} from '@lexical/html'
+import {getCurrentDocID} from "./utils.ts";
+import {get, onValue, ref, update} from "firebase/database";
+import {database} from "./firebase.ts";
+import {$getRoot, $insertNodes} from "lexical";
+import {useDebouncedCallback} from "use-debounce";
 
 const theme = {
     // Theme styling goes here
@@ -25,11 +27,6 @@ function onError(error: unknown) {
 }
 
 function Editor() {
-    const onChange = (editorState: EditorState) => {
-        console.log(editorState.toJSON());
-        // console.log(JSON.stringify(editorState.toJSON(), null, 2))
-    }
-
     const initialConfig = {
         namespace: 'MyEditor',
         theme,
@@ -40,7 +37,7 @@ function Editor() {
         <LexicalComposer initialConfig={initialConfig}>
             <div className="editor-container">
                 <div className="editor-inner">
-                    <RichTextPlugin
+                    <PlainTextPlugin
                         contentEditable={
                             <ContentEditable
                                 className="editor-input"
@@ -50,9 +47,9 @@ function Editor() {
                         }
                         ErrorBoundary={LexicalErrorBoundary}
                     />
-                    <HistoryPlugin />
-                    <AutoFocusPlugin />
-                    <OnChangePlugin onChange={onChange}/>
+                    <HistoryPlugin/>
+                    <AutoFocusPlugin/>
+                    <Firebase/>
                 </div>
             </div>
         </LexicalComposer>
@@ -60,3 +57,63 @@ function Editor() {
 }
 
 export default Editor;
+
+function Firebase() {
+    const [editor] = useLexicalComposerContext()
+    const docId = getCurrentDocID();
+    const debouncedSave = useDebouncedCallback((value: string) => {
+        return update(ref(database, docId), {text: value})
+    }, 2000, {maxWait: 5000})
+
+    useEffect(() => {
+        let unsub: ReturnType<typeof onValue> | undefined;
+        let firstLoad = true;
+
+        (async () => {
+            editor.setEditable(false);
+            const docRef = ref(database, docId)
+            const docSnap = await get(docRef)
+            if (docSnap.exists()) {
+                editor.update(() => {
+                    const parser = new DOMParser();
+                    const dom = parser.parseFromString(docSnap.val().text, "text/html")
+                    const nodes = $generateNodesFromDOM(editor, dom);
+
+                    // Select the root
+                    $getRoot().select();
+                    $getRoot().clear();
+
+                    // Insert them at a selection.
+                    $insertNodes(nodes);
+                })
+            }
+        })()
+            .then(() => {
+                firstLoad = false;
+                editor.setEditable(true);
+                editor.focus();
+            })
+            .catch((err) => {
+                alert("Error initializing editor");
+                console.error(err);
+            });
+
+        const onUpdateCleanup = editor.registerUpdateListener(() => {
+            if (firstLoad) {
+                return;
+            }
+            editor.update(() => {
+                debouncedSave($generateHtmlFromNodes(editor))
+            })
+        });
+        return () => {
+            onUpdateCleanup()
+            debouncedSave.flush();
+            if (unsub) {
+                unsub()
+            }
+        }
+    }, [debouncedSave, docId, editor]);
+
+    return null
+}
